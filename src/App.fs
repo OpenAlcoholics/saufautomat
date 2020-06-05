d8cd78f4a2fa5ba137f9715321c3f689278bc965
Torben Carstens <github@carstens.tech>
2020-06-05 14:01:11 +0200

    Update url for cards

--- src/App.fs
+++ src/App.fs
@@ -51,7 +51,7 @@ type Msg =
 
 let getCards dispatch =
     promise {
-        let url = "https://raw.githubusercontent.com/torbencarstens/drinking-game-cards/develop/tasks_is.json"
+        let url = "https://raw.githubusercontent.com/OpenAlcoholics/drinking-game-cards/develop/tasks_is.json"
         let! res = Fetch.get (url)
         AddCards res |> dispatch
     }
