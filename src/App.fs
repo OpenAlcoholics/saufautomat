f01a1d8df21f3f2d8af0ddb420b82cc16adfeb95
Torben Carstens <github@carstens.tech>
2020-06-06 18:44:08 +0200

    New design (dark mode)

--- src/App.fs
+++ src/App.fs
@@ -32,6 +32,7 @@ type RawCard =
 
 type Model =
     { Players: Player list
+      ActiveCards: RawCard list
       CurrentCard: RawCard option
       Cards: RawCard list
       CurrentPlayer: Player option
@@ -44,6 +45,7 @@ type Msg =
     | ChangeActiveCard
     | ChangeActivePlayer
     | IncrementCounter
+    | UpdateActiveCard of RawCard
     | AddCards of RawCard list
     | AddPlayer of Player
     | RemovePlayer of Player
@@ -60,6 +62,7 @@ let getCards dispatch =
 
 let init (): Model * Cmd<Msg> =
     { Players = List.empty
+      ActiveCards = List.empty
       CurrentCard = None
       Cards = List.empty
       CurrentPlayer = None
@@ -93,25 +96,21 @@ let rec findNextActivePlayer (playerList: Player list) model =
         | None -> Some model.Players.Head
 
 let unwrapOr (opt: 'b option) (m: 'b -> 't) (def: 't) =
-    if opt.IsSome then
-        m opt.Value
-    else
-        def
+    if opt.IsSome then m opt.Value else def
 
 // UPDATE
 
 let getNextCard model =
+    let distinctCount = (getDistinctCardCount model.Cards)
+
     let cards =
         List.filter (fun card ->
-            card.count > 0
-            && if model.Players.Length = 0 then
-                not card.personal
-               else
-                   true && if (getDistinctCardCount model.Cards) > 1 then card.text <> (unwrapOr
-                                                                                            model.CurrentCard
-                                                                                            (fun c -> c.text)
-                                                                                            "") else true)
-            model.Cards
+            card.count > 0 && if model.Players.Length = 0 then
+                                  not card.personal
+                              else
+                                  true && if distinctCount > 1
+                                          then card.text <> (unwrapOr model.CurrentCard (fun c -> c.text) "")
+                                          else true) model.Cards
 
     if cards.Length = 0 then
         None
@@ -182,6 +181,7 @@ let update (msg: Msg) (model: Model) =
                       if p.Name = player.Name then { p with Active = not p.Active } else p) model.Players) }, Cmd.Empty
     | DisplayPlayerNameDuplicate -> { model with DisplayPlayerNameDuplicateError = true }, Cmd.Empty
     | HidePlayerNameDuplicate -> { model with DisplayPlayerNameDuplicateError = false }, Cmd.Empty
+    | UpdateActiveCard card -> model, Cmd.Empty // TODO
 
 // VIEW (rendered with React)
 
@@ -202,20 +202,21 @@ let inline (|?) (a: 'a option) b =
     if a.IsSome then a.Value else b
 
 let displayPlayer player model dispatch =
-    div [ ClassName "card" ]
-        [ div
-            [ Id
-                (if model.CurrentPlayer.IsSome && model.CurrentPlayer.Value.Name = player.Name
-                 then "player-current"
-                 else "")
-              ClassName("card-body " + if player.Active then "player-active" else "player-inactive") ]
+    div
+        [ ClassName
+            ("flex-column mr-2 card " + (if model.CurrentPlayer.IsSome && model.CurrentPlayer.Value.Name = player.Name
+                                    then "bg-primary"
+                                    else if not player.Active
+                                    then "border-warning"
+                                    else "")) ]
+        [ div [ ClassName "card-body" ]
               [ h5 [ ClassName "card-title text-center" ] [ str player.Name ]
-                div [ ClassName "d-flex justify-content-around" ]
+                div [ ClassName "d-flex justify-content-between" ]
                     [ button
-                        [ ClassName "card-text"
+                        [ ClassName "card-text btn btn-secondary toggle-button"
                           OnClick(fun _ -> TogglePlayerActivity player |> dispatch) ] [ str "Toggle" ]
                       button
-                          [ ClassName "card-text"
+                          [ ClassName "card-text btn btn-secondary delete-button"
                             OnClick(fun _ ->
                                 do RemovePlayer player |> dispatch
                                    ChangeActivePlayer |> dispatch) ] [ str "Delete" ] ] ] ]
@@ -236,54 +237,97 @@ let addPlayerFunction model dispatch =
         |> ignore
 
 let sidebar (model: Model) dispatch =
-    div [ ClassName "col-md-2 bg-light sidebar" ]
-        [ div []
-              [ input
-                  [ Name "add-player-field"
-                    Id "add-player-field"
-                    OnKeyDown(fun x ->
-                        if x.keyCode = 13. then (addPlayerFunction model dispatch)) ]
-                button
-                    [ ClassName "align-self-center"
-                      OnClick(fun _ -> addPlayerFunction model dispatch) ] [ str "Add player" ] ]
+    div
+        [ ClassName "col-md-2 sidebar col h-100" ]
+        [ div [ ClassName "form-group" ]
+                    [ input
+                        [ Name "add-player-field"
+                          ClassName "form-control m-1 w-100"
+                          Id "add-player-field"
+                          OnKeyDown(fun x ->
+                              if x.keyCode = 13. then (addPlayerFunction model dispatch))
+                          MaxLength 20. ]
+                      button
+                        [ ClassName "btn btn-primary m-1 w-100"
+                          OnClick(fun _ -> addPlayerFunction model dispatch) ] [ str "Add player" ] ]
           hr []
-          div [ ClassName "card" ] (List.map (fun p -> displayPlayer p model dispatch) model.Players) ]
+          div [ ClassName "flex-row mb-4"
+                Style [ OverflowY "scroll"
+                        Height "85%" ] ] (List.map (fun p -> displayPlayer p model dispatch) model.Players) ]
+
+let displayCurrentCard model dispatch =
+    div
+        [ ClassName "card d-flex col"
+          Id "active-card" ]
+        [ div [ ClassName "card-body flex-wrap" ]
+              [ button
+                  [ OnClick(fun _ ->
+                      do ChangeActiveCard |> dispatch
+                         ChangeActivePlayer |> dispatch)
+                    ClassName "card-body card-title btn btn-dark w-100 h-100"
+                    Id "current-card-body"
+                    Disabled(model.CurrentCard.IsNone && model.Counter > 0) ]
+                    [ span [ ClassName "h3" ]
+                          [ str
+                              (match model.CurrentCard with
+                               | Some (card) -> card.text
+                               | None ->
+                                   if model.Counter = 0 then "Click to start" else "No cards left") ] ] ] ]
+
+type HtmlAttr =
+    | [<CompiledName("aria-valuenow")>] AriaValueNow of string
+    | [<CompiledName("aria-valuemin")>] AriaValueMin of string
+    | [<CompiledName("aria-valuemax")>] AriaValueMax of string
+    interface IHTMLProp
+
+let displayInformationHeader model dispatch =
+    div
+        [ Id "active-player-header"
+          ClassName "text-center col text-truncate h3" ]
+        [ span []
+              [ str
+                  (match model.CurrentPlayer with
+                   | Some player -> player.Name
+                   | None -> "No active player") ]
+          span [] [ str " | " ]
+          span [ Title "Number of cards played so far" ] [ str (sprintf "%d" model.Counter) ]
+          span [] [ str " / " ]
+          span [ Title "Total number of cards" ] [ str (sprintf "%d " model.Cards.Length) ]
+          span [] [ str " " ]
+          span [ Title "Distinct number of cards" ] [ str (sprintf "(%d)" (getDistinctCardCount model.Cards)) ]
+          div [ ClassName "progress" ]
+              [ div
+                  [ ClassName "progress-bar"
+                    Role "progressbar"
+                    Style([ Width(sprintf "%d%%" ((model.Counter * 100 / model.Cards.Length))) ])
+                    AriaValueNow(sprintf "%d" model.Counter)
+                    AriaValueMin "0"
+                    AriaValueMax(sprintf "%d" model.Cards.Length) ] [] ] ]
+
+let displayActiveCard (card: RawCard) model dispatch =
+    div [ ClassName "card p-2 m-1"
+          Title card.text ] [ h5 [ ClassName "card-title h-100" ] [ str card.text ] ]
+
+let activeCards (model: Model) dispatch =
+    div
+        [ ClassName "active-cards row mr-4 mt-3"
+          Style [ Height "22%"
+                  OverflowY "scroll" ] ]
+        [ div [ ClassName "col d-flex flex-wrap" ] (List.map (fun card -> displayActiveCard card model dispatch) model.ActiveCards) ]
 
 let view (model: Model) dispatch =
     div
         [ Ref(fun element ->
             if not (isNull element) then
-                if model.InitialLoad then dispatch InitialLoad) ]
-        [ (sidebar model dispatch)
-          p
-              [ Id "active-player-header"
-                ClassName "text-center" ]
-              [ span []
-                    [ str
-                        (match model.CurrentPlayer with
-                         | Some player -> player.Name
-                         | None -> "No active player") ]
-                span [] [ str " | " ]
-                span [ Title "Number of cards played so far" ] [ str (sprintf "%d" model.Counter) ]
-                span [] [ str " / " ]
-                span [ Title "Total number of cards" ] [ str (sprintf "%d " model.Cards.Length) ]
-                span [] [ str " " ]
-                span [ Title "Distinct number of cards" ]
-                    [ str (sprintf "(%d)" (getDistinctCardCount model.Cards)) ] ]
+                if model.InitialLoad then dispatch InitialLoad)
+          ClassName "container-fluid h-100" ]
+        [ div [ ClassName "row m-4" ] [ (displayInformationHeader model dispatch) ]
           div
-              [ ClassName "card"
-                Id "card" ]
-              [ button
-                  [ OnClick(fun _ ->
-                      do ChangeActiveCard |> dispatch
-                         ChangeActivePlayer |> dispatch)
-                    ClassName "card-body card-title"
-                    Disabled(model.CurrentCard.IsNone && model.Counter > 0) ]
-                    [ str
-                        (match model.CurrentCard with
-                         | Some (card) -> card.text
-                         | None ->
-                             if model.Counter = 0 then "Click to start" else "No cards left") ] ] ]
+              [ ClassName "row m-2"
+                Style [ Height "65%" ] ]
+              [ (displayCurrentCard model dispatch)
+                (sidebar model dispatch) ]
+          (activeCards model dispatch) ]
 
 // App
 Program.mkProgram init update view
